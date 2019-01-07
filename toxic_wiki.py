fd8630214fc721ca92d9a6e0b245894191830e91
C. Thomas Brittain <cthomasbrittain@hotmail.com>
2019-01-07 17:02:21 -0600

    Working CNN toxic_comment classifer

--- toxic_wiki.py
+++ toxic_wiki.py
@@ -37,10 +37,7 @@ import pandas as pd
 ############################
 
 BASE_DIR = '/Users/cthomasbrittain/dl-nlp/data/'
-GLOVE_DIR = os.path.join(BASE_DIR)
-GOOGLE_NEWS_PATH = BASE_DIR + 'GoogleNews-vectors-negative300.bin'
-TRAIN_TEXT_DATA_DIR = BASE_DIR + 'toxic-comment/test.csv'
-TRAIN_TEXT_LABELS_DIR = BASE_DIR + 'toxic-comment/test_labels.csv'
+TRAIN_TEXT_DATA_DIR = BASE_DIR + 'toxic-comment/train.csv'
 MAX_SEQUENCE_LENGTH = 100
 MAX_NUM_WORDS = 20000
 EMBEDDING_DIM = 25
@@ -53,8 +50,7 @@ VALIDATION_SPLIT = 0.2
 print('Loading word vectors.')
 
 # Load embeddings
-model = gensim.models.KeyedVectors.load_word2vec_format(GOOGLE_NEWS_PATH, binary=True)
-#info = api.info()                       # show info about available models/datasets
+info = api.info()                       # show info about available models/datasets
 model = api.load("glove-twitter-25")    # download the model and return as object ready for use
 
 vocab_size = len(model.vocab)
@@ -69,56 +65,67 @@ for index in range(vocab_size):
 ############################################
 # Get labels
 ############################################
+    
+with open(TRAIN_TEXT_DATA_DIR) as f:
+    toxic_comments = pd.read_csv(TRAIN_TEXT_DATA_DIR)
 
-print('Preparing embedding matrix.')
+print('Getting Comment Labels.')
 labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-with open(TRAIN_TEXT_LABELS_DIR) as f:
-    y = pd.read_csv(TRAIN_TEXT_LABELS_DIR)[labels].values
+with open(TRAIN_TEXT_DATA_DIR) as f:
+    label = toxic_comments[labels].values
     
 ############################################
-# Convert Toxic Comments to Word Vectors
+# Convert Toxic Comments to Sequences
 ############################################
 
 print('Processing text dataset')
 
-with open(TRAIN_TEXT_LABELS_DIR) as f:    
-    toxic_comments = pd.read_csv(TRAIN_TEXT_DATA_DIR)
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(toxic_comments['comment_text'].fillna("DUMMY_VALUE").values)
+sequences = tokenizer.texts_to_sequences(toxic_comments['comment_text'].fillna("DUMMY_VALUE").values)
 
-comment_vectors = []
-with open(TRAIN_TEXT_DATA_DIR) as f:
-    for index, text in enumerate(toxic_comments['comment_text'].tolist()):
-        words = text.split()
-        word_vec = []
-        for word in words:
-            try:
-                word_vec.append(word2index[word])
-            except:
-                continue
-        comment_vectors.append(word_vec)
+word_index = tokenizer.word_index
 
-print('Found %s comments.' % len(comment_vectors))
+print('Found %s sequences.' % len(sequences))
 
 
 ############################################
 # Pad Sequences
 ############################################
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 
-data = pad_sequences(comment_vectors, maxlen=MAX_SEQUENCE_LENGTH)
+
+###################################################
+# Test / Train Split
+###################################################
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-nb_validation_samples]
+y_train = labels[:-nb_validation_samples]
+x_val = data[-nb_validation_samples:]
+y_val = labels[-nb_validation_samples:]
 
 
-############################################
+###################################################
 # Convert Toxic Comment Vectors to Embedding Layer
-############################################
+###################################################
 
 #for index, row in enumerate(data):
 #    embedding_matrix[index] = model.get_vector()
-
-embedding_matrix = np.zeros((len(word2index) + 1, EMBEDDING_DIM))
-for word, i in word2index.items():
-    embedding_vector = model.get_vector(word)
-    if embedding_vector is not None:
-        # words not found in embedding index will be all-zeros.
-        embedding_matrix[i] = embedding_vector
+num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+for word, i in word_index.items():
+    try:
+        embedding_vector = model.get_vector(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    except:
+        continue
 
 # load pre-trained word embeddings into an Embedding layer
 # note that we set trainable = False so as to keep the embeddings fixed
@@ -134,23 +141,26 @@ embedding_layer = Embedding(len(word2index),
 
 print('Training model.')
 
-# train a 1D convnet with global maxpooling
+
+print('Building model...')
+
 sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = embeddings(sequence_input)
+embedded_sequences = embedding_layer(sequence_input)
 x = Conv1D(128, 5, activation='relu')(embedded_sequences)
 x = MaxPooling1D(5)(x)
 x = Conv1D(128, 5, activation='relu')(x)
 x = MaxPooling1D(5)(x)
-x = Conv1D(128, 3, activation='relu')(x)
-x = GlobalMaxPooling1D()(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(35)(x)  # global max pooling
+x = Flatten()(x)
 x = Dense(128, activation='relu')(x)
-preds = Dense(len(labels), activation='softmax')(x)
+preds = Dense(len(labels_index), activation='softmax')(x)
 
 model = Model(sequence_input, preds)
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['acc'])
 
-model.fit(data, y,
-          batch_size=128,
-          epochs=10)
+# happy learning!
+model.fit(x_train, y_train, validation_data=(x_val, y_val),
+          epochs=2, batch_size=128)
